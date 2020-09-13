85b2f26921d0ed5a7abc5dc1e98ece4f50a846d4
christian.celora@gmail.com <christian.celora@gmail.com>
2020-09-13 14:58:23 +0200

    fixed require dropzone js

--- public/js/upload_pdf.js
+++ public/js/upload_pdf.js
@@ -3658,27 +3658,16 @@ module.exports = function(module) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! dropzone/index.js */ "./node_modules/dropzone/index.js"); // var CSRF_TOKEN = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
-// // Dropzone.autoDiscover = false;
-// var myDropzone = new Dropzone(".dropzone",{ 
-//     acceptedFiles: ".pdf",
-// });
-// myDropzone.on("sending", function(file, xhr, formData) {
-//     formData.append("_token", CSRF_TOKEN);
-// }); 
-// Dropzone.options.myAwesomeDropzone = {
-//     accept: function(file, done) {
-//         console.log("uploaded");
-//         done();
-//     },
-//     init: function() {
-//         this.on("addedfile", function() {
-//             if (this.files[1]!=null){
-//                 this.removeFile(this.files[0]);
-//             }
-//         });
-//     }
-// };
+var Dropzone = __webpack_require__(/*! dropzone/index.js */ "./node_modules/dropzone/index.js");
+
+Dropzone.options.myAwesomeDropzone = {
+  maxFiles: 1,
+  acceptedFiles: ".pdf",
+  accept: function accept(file, done) {
+    console.log("uploaded");
+    done();
+  }
+};
 
 /***/ }),
 
--- resources/js/upload_pdf.js
+++ resources/js/upload_pdf.js
@@ -1,24 +1,10 @@
-require('dropzone/index.js');
+const Dropzone = require('dropzone/index.js');
 
-// var CSRF_TOKEN = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
-// // Dropzone.autoDiscover = false;
-// var myDropzone = new Dropzone(".dropzone",{ 
-//     acceptedFiles: ".pdf",
-// });
-// myDropzone.on("sending", function(file, xhr, formData) {
-//     formData.append("_token", CSRF_TOKEN);
-// }); 
-
-// Dropzone.options.myAwesomeDropzone = {
-//     accept: function(file, done) {
-//         console.log("uploaded");
-//         done();
-//     },
-//     init: function() {
-//         this.on("addedfile", function() {
-//             if (this.files[1]!=null){
-//                 this.removeFile(this.files[0]);
-//             }
-//         });
-//     }
-// };
+Dropzone.options.myAwesomeDropzone = {
+    maxFiles: 1,
+    acceptedFiles: ".pdf",
+    accept: function(file, done) {
+        console.log("uploaded");
+        done();
+    }
+};
