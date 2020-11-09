e0369b3da32c0052b282298c295e0aae3dcd633b
christian.celora@gmail.com <christian.celora@gmail.com>
2020-11-09 10:55:21 +0100

    fix convert after blue/green tests

--- app/Http/Controllers/ConverterPdfController.php
+++ app/Http/Controllers/ConverterPdfController.php
@@ -12,7 +12,7 @@ abstract class ConverterPdfController extends PdfController {
         $file = $request->file("file");
         $filename = pathinfo($file, PATHINFO_FILENAME);
         $client_filename = substr($file->getClientOriginalName(), 0, strrpos($file->getClientOriginalName(), ".")).".pdf";
-        $converter = ConverterFactory::create($file->path(), ConverterFactory::PDF_CONVERTER, true);
+        $converter = ConverterFactory::create($file->path(), ConverterFactory::PDF_CONVERTER);
         $output = $converter->convert();
         Storage::disk("public")->put($output, file_get_contents($output));
         return array(
